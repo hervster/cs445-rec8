5f71ae6870cdd52d8f2765b57f6af9d79260da4d
William C. Garrison III <bill@cs.pitt.edu>
2021-07-07 18:01:36 -0400

    Adjusted test formatting

--- app/src/test/java/cs445/rec8/test/SortTest.java
+++ app/src/test/java/cs445/rec8/test/SortTest.java
@@ -9,31 +9,31 @@ import cs445.rec8.Sorting;
 
 public class SortTest{
 
-	Integer[] a;
-    final String sep = ",";
+    Integer[] a;
 
-	@AfterEach
-	public void teardown(){
-		a = null;
-	}
+    @AfterEach
+    public void teardown(){
+        a = null;
+    }
 
-	@Test
-	@DisplayName("Test QuickSortHybrid")
-	void testQuicksort_Hybrid(){
+    @Test
+    @DisplayName("Test QuickSortHybrid")
+    void testQuicksort_Hybrid(){
 
-		int minSize = 100;
-		int maxSize = 5000;
-		int step = 100;
-		int baseCase = 20;
+        int minSize = 100;
+        int maxSize = 5000;
+        int step = 100;
+        int baseCase = 20;
 
-		for (int i = minSize; i <= maxSize; i+=step) {
-			a = Sorting.buildArray(i);
+        for (int i = minSize; i <= maxSize; i+=step) {
+            a = Sorting.buildArray(i);
 
-			Sorting.shuffle(a);
-			Sorting.hybridQuickSort(a, baseCase);
-			assert Helper.isSorted(a);
-		}
+            Sorting.shuffle(a);
+            Sorting.hybridQuickSort(a, baseCase);
+            assert Helper.isSorted(a);
+        }
 
-	}
+    }
+
 }
 
